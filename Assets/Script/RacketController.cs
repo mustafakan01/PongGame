aeba0ab3619f896244372091a60a04f4b3e05ab2
Mustafa Kan <mustafakan242@gmail.com>
2024-04-24 12:13:35 +0300

    Game

--- Assets/Script/RacketController.cs
+++ Assets/Script/RacketController.cs
@@ -1,44 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RacketController : MonoBehaviour
 {
     public float speed;
-    public float limitHorizontal=2.2f;
+    public float limitHorizontal = 2.2f;
     public bool isUp;
     [SerializeField] private bool isPlayer;
 
     [SerializeField] private float aiSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     private void Update()
     {
-        Vector3 newPoz=transform.position;
+        Vector3 newPosition = transform.position;
+
         if (isPlayer)
         {
-            var input = Input.GetAxis("Horizontal");
-
-            newPoz = transform.position + (Vector3.right * (input * speed * Time.deltaTime));
-           
+            // Mobil cihazlarda dokunmatik giriþi kullan
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                float touchPositionX = Camera.main.ScreenToWorldPoint(touch.position).x;
+                newPosition.x = Mathf.Clamp(touchPositionX, -limitHorizontal, limitHorizontal);
+            }
         }
-        else 
+        else
         {
-            var ai = Mathf.Lerp(newPoz.x, Ball.Instance.transform.position.x,aiSpeed*Time.deltaTime) ;
-            newPoz.x = ai;
+            // AI'nýn topun pozisyonunu takip etme hýzýný ayarla
+            float aiTargetX = Mathf.Lerp(newPosition.x, Ball.Instance.transform.position.x, aiSpeed * Time.deltaTime);
+            newPosition.x = aiTargetX;
         }
 
-        newPoz.x = Mathf.Clamp(newPoz.x, -limitHorizontal, limitHorizontal);
-
-        transform.position = newPoz;
-
-
-
+        // Yeni pozisyonu güncelle
+        transform.position = newPosition;
     }
 }
